012adc7bb16fc19df14957c30451a70d63a59ca5
Jack Briggs <briggs.jsa@gmail.com>
2021-05-12 19:22:50 +0100

    create message generator

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ## Scope
 
 Mixed Messages is a JavaScript program to generate a random message whenever the program is run.
+
+## Message information
+
+The random message that is returned happens to be in a language that nobody speaks.
+I had thought about returning random words in a specified language but didn't know how to or really want to inport multiple complete dictionaries.
--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
-
-//console.log(letters[3]);
+const messageLength = 12
 
 //Word generator
 const wordGen = () => {
@@ -20,6 +19,22 @@ const wordGen = () => {
     return wholeWord;
 };
 
-//console.log(wordGen());
 
+//Message generator
+const messageGen = () => {
+    let messageCreation = [];
+    for (x = 0; x <= messageLength; x ++) {
+        const word = wordGen();
+        messageCreation.push(word);
+    };
 
+    //Join words
+    const joinedMessage = messageCreation.join(' ');
+    
+    //Capitalise first word
+    const capitalisedMessage = joinedMessage[0].toUpperCase() + joinedMessage.slice(1) + '.';
+
+    return capitalisedMessage
+}
+
+console.log(messageGen());
